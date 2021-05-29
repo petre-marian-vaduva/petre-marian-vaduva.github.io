8b19ec8470328636cc234be24a5e58cb6ca31716
Petre-Marian Vaduva <rik_296@yahoo.com>
2021-05-29 12:33:42 +0100

    add function active links

--- portfolio.js
+++ portfolio.js
@@ -17,3 +17,21 @@ function linkAction() {
     navMenu.classList.remove("show")
 }
 navLink.forEach(n => n.addEventListener("click", linkAction))
+
+const sections = document.querySelectorAll("section[id]")
+window.addEventListener("scroll", scrollActive)
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+    sections.forEach(current =>{
+        const sectionHeight = curent.offsetHeight
+        const sectionTop = curent.offsetTop - 50;
+        sectionId = current.getAtribute("id")
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active')
+        }
+    })
+}
