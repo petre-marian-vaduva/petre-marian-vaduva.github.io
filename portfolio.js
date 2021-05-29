cc6dc352de5ea28923313d827288940ab953eee2
Petre-Marian Vaduva <rik_296@yahoo.com>
2021-05-29 12:42:24 +0100

    add animation for PORTFOLIO section

--- portfolio.js
+++ portfolio.js
@@ -56,3 +56,6 @@ sr.reveal(".about__social-icon", {delay: 600, interval: 200})
 sr.reveal(".skills__subtitle", {}) 
 sr.reveal(".skills__name", {distance: "20px", delay: "50", interval: 100}) 
 sr.reveal(".skills__img", {delay: 400})
+
+
+sr.reveal(".portofolio__img", {interval: 200})
